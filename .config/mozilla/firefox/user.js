a36af4616580b1da6272a409a5a93c5b3075965f
Homero Pawlowski <homeski2@gmail.com>
2026-05-19 18:28:45 -0700

    Add firefox settings

---
+++ .config/mozilla/firefox/user.js
@@ -0,0 +1,36 @@
+// On-device AI settings
+user_pref("browser.ai.control.linkPreviewKeyPoints", "blocked");
+user_pref("browser.ai.control.pdfjsAltText", "blocked");
+user_pref("browser.ml.linkPreview.enabled", false);
+user_pref("browser.ml.linkPreview.optin", false);
+user_pref("browser.tabs.groups.smart.userEnabled", false);
+user_pref("pdfjs.enableAltText", false);
+// New tab settings
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.addons", false);
+user_pref("browser.newtabpage.activity-stream.asrouter.userprefs.cfr.features", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.highlights", false);
+user_pref("browser.newtabpage.activity-stream.feeds.section.topstories", false);
+user_pref("browser.newtabpage.activity-stream.feeds.topsites", false);
+user_pref("browser.newtabpage.activity-stream.showSponsored", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredCheckboxes", false);
+user_pref("browser.newtabpage.activity-stream.showSponsoredTopSites", false);
+user_pref("browser.newtabpage.activity-stream.showWeather", false);
+// Bookmarks bar
+user_pref("browser.toolbars.bookmarks.visibility", "always");
+// Password suggestions
+user_pref("signon.rememberSignons", false);
+// Data collection
+user_pref("browser.crashReports.unsubmittedCheck.autoSubmit2", false);
+user_pref("browser.ipProtection.enabled", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.merino-provider.ohttp.enabled", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.promoCard.enabled", false);
+user_pref("browser.newtabpage.activity-stream.discoverystream.sections.personalization.inferred.enabled", false);
+user_pref("browser.newtabpage.activity-stream.telemetry.privatePing.inferredInterests.enabled", false);
+user_pref("datareporting.healthreport.uploadEnabled", false);
+user_pref("datareporting.usage.uploadEnabled", false);
+user_pref("nimbus.rollouts.enabled", false);
+user_pref("nimbus.profilesdatastoreservice.read.enabled", false);
+user_pref("nimbus.profilesdatastoreservice.sync.enabled", false);
+// Firefox suggest
+user_pref("browser.urlbar.suggest.quicksuggest.all", false);
+user_pref("browser.urlbar.suggest.trending", false);
